41e11cf07372de2b46a58fbd8601015a48b078de
xuhaoddd <49242620+xuhaoddd@users.noreply.github.com>
2019-09-17 00:25:30 +0900

    Update basic_image_operation1.py

--- opencv-code/basic_image_operation1.py
+++ opencv-code/basic_image_operation1.py
@@ -1,4 +1,4 @@
-import cv2
+'''import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -31,30 +31,33 @@ dst=cv2.addWeighted(img,0.7,img1,0.3,0) #dst = α · img + β · img1 + γ
 cv2.imshow('blend',dst)
 cv2.waitKey(0)
 cv2.destroyAllWindow()
-
-# threshold https://blog.csdn.net/on2way/article/details/46812121
+'''
+# 
 import cv2
 import numpy as np
-# 加?图像
-img1 = cv2.imread('roi.jpg')
-img2 = cv2.imread('opencv_logo.png')
-# I want to put logo on top-left corner, So I create a ROI
+
+img1 = cv2.imread('E:/test image/Lena.png')
+img2 = cv2.imread('E:/test image/opencv_logo.png')
+
 rows,cols,channels = img2.shape
 roi = img1[0:rows, 0:cols ]
-# Now create a mask of logo and create its inverse mask also
+
 img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(img2gray, 175, 255, cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
-# Now black-out the area of logo in ROI
-# 取 roi 中与 mask 中不为?的值对应的像素的值?其他值为 0
-# 注意??必?有 mask=mask 或者 mask=mask_inv, 其中的 mask= 不能忽略
+
 img1_bg = cv2.bitwise_and(roi,roi,mask = mask)
-# 取 roi 中与 mask_inv 中不为?的值对应的像素的值?其他值为 0 。
-# Take only region of logo from logo image.
+
 img2_fg = cv2.bitwise_and(img2,img2,mask = mask_inv)
-# Put logo in ROI and modify the main image
+
 dst = cv2.add(img1_bg,img2_fg)
 img1[0:rows, 0:cols ] = dst
+cv2.imshow('img1_bg',img1_bg)
+cv2.imshow('img2_fg',img2_fg)
+cv2.imshow('img2gray',img2gray)
+cv2.imshow('mask',mask)
 cv2.imshow('res',img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
